ba5c3c2c33617fc7b006324bd0f19a1d488c3d33
RabeyaMuna <rabeya.khatun.muna@g.bracu.ac.bd>
2025-11-19 05:53:17 -0500

    RabeyaMuna__diff__id_288

--- tests/functional/customer/test_order_status.py
+++ tests/functional/customer/test_order_status.py
@@ -7,6 +7,7 @@ from oscar.test.testcases import WebTestCase
 
 
 class TestAnAnonymousUser(WebTestCase):
+    # Note: UserFactory._after_postgeneration will stop saving the instance after postgeneration hooks in the next major release.
     def test_gets_a_404_when_requesting_an_unknown_order(self):
         path = reverse(
             "customer:anon-order", kwargs={"order_number": 1000, "hash": "1231231232"}
@@ -21,7 +22,9 @@ class TestAnAnonymousUser(WebTestCase):
             kwargs={"order_number": order.number, "hash": order.verification_hash()},
         )
         response = self.app.get(path)
-        self.assertEqual(http_client.OK, response.status_code)
+        self.assertEqual(
+            http_client.OK, response.status_code, "Expected HTTP 200 OK status code."
+        )
 
     def test_gets_404_when_using_incorrect_hash(self):
         order = create_order()
